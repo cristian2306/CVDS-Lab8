67317be5e21c72e068ec312ff35b82bde10907be
diego gonzalez <diego.gonzalez-g@mail.escuelaing.edu.co>
2021-10-13 18:53:52 -0500

    Commit capa de aplicacion

--- MyBatis_Introduction_VideoRental/src/main/java/edu/eci/cvds/guice/GuiceContextListener.java
+++ MyBatis_Introduction_VideoRental/src/main/java/edu/eci/cvds/guice/GuiceContextListener.java
@@ -1,5 +1,42 @@
 package edu.eci.cvds.guice;
 
-public class GuiceContextListener {
-    
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import org.mybatis.guice.XMLMyBatisModule;
+import org.mybatis.guice.datasource.helper.JdbcHelper;
+
+import edu.eci.cvds.sampleprj.dao.*;
+import edu.eci.cvds.sampleprj.dao.mybatis.*;
+import edu.eci.cvds.samples.services.*;
+import edu.eci.cvds.samples.services.impl.*;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class GuiceContextListener implements ServletContextListener {
+
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        servletContext.removeAttribute(Injector.class.getName());
+    }
+
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        Injector injector = Guice.createInjector(new XMLMyBatisModule() {
+            @Override
+            protected void initialize() {
+                install(JdbcHelper.MySQL);
+                setEnvironmentId("development");
+                setClassPathResource("mybatis-config.xml");
+
+                bind(ItemDAO.class).to(MyBATISItemDAO.class);
+                bind(ClienteDAO.class).to(MyBATISClienteDAO.class);
+                bind(TipoItemDAO.class).to(MyBATISTipoItemDAO.class);
+                bind(ItemRentadoDAO.class).to(MyBATISItemRentadoDAO.class);
+                bind(ServiciosAlquiler.class).to(ServiciosAlquilerImpl.class);
+            }
+        });
+
+        servletContextEvent.getServletContext().setAttribute(Injector.class.getName(), injector);
+    }
 }
